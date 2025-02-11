7909a726246549c0b9591f18cd88e6c73a4570a2
prasad <d6.dreamsix@gmail.com>
2025-02-11 12:22:29 +0530

    Registration Styles chnages

--- src/Components/Registration/Registration.jsx
+++ src/Components/Registration/Registration.jsx
@@ -8,82 +8,71 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const Registration = () => {
-  const navigate = useNavigate()
-  const [roleList, setRoleList] = useState([])
+  const navigate = useNavigate();
+  const [roleList, setRoleList] = useState([]);
 
   useEffect(() => {
-    getRoleList()
-  }, [])
+    getRoleList();
+  }, []);
 
   const getRoleList = () => {
-    axios.get("http://localhost:8080/api/role/all")
-    .then(response => {
-      console.log('response', response)
-      setRoleList(response?.data?.totalContent)
-    })
-    .catch(error => {
-      console.error("Error:", error);
-    })
+    axios
+      .get("http://localhost:8080/api/role/all")
+      .then((response) => {
+        console.log("response", response);
+        setRoleList(response?.data?.totalContent);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
+
   const onFinish = (values) => {
-    if (values.password !== values.confirmPassword){
-      return message.error("Password and confirm password should be equal")
+    if (values.password !== values.confirmPassword) {
+      return message.error("Password and confirm password should be equal");
     }
-    const role = roleList?.filter(role => role.roleName === "USER")
+    const role = roleList?.filter((role) => role.roleName === "USER");
     const payload = {
       ...values,
-      roles: role?.id
-    }
-    userService.createUser(payload)
-      .then(response => {
-        navigate("/loginpage")
+      roles: role?.id,
+    };
+    userService
+      .createUser(payload)
+      .then((response) => {
+        navigate("/loginpage");
       })
-      .catch(error => {
-        console.log('error', error)
-      })
+      .catch((error) => {
+        console.log("error", error);
+      });
   };
 
   return (
     <div className="register-container">
+      <Form className="register-form" onFinish={onFinish} layout="vertical">
       <div className="register-logo">
         <img src={Logo} alt="DreamSix Logo" />
       </div>
-      <h2>Hello! Register to get started</h2>
-      <Form className="register-form" onFinish={onFinish} layout="vertical">
-        <Form.Item
-         
-          name="name"
-          rules={[{ required: true, message: "Please enter your full name!" }]}
-        >
-          <Input placeholder="Full Name"  style={{height:50}}/>
+      <h2>Welcome to DreamSix !</h2>
+        <Form.Item name="name" rules={[{ required: true, message: "Please enter your full name!" }]}> 
+          <Input placeholder="Full Name" />
         </Form.Item>
-        <Form.Item
-          
-          name="phoneNumber"
-          rules={[
-            { required: true, message: "Please enter your WhatsApp number!" },
-            { pattern: /^\d{10}$/, message: "Enter a valid 10-digit number!" },
-          ]}
-        >
-          <Input placeholder="WhatsApp Number"style={{height:50}} />
+        <Form.Item name="phoneNumber" rules={[{ required: true, message: "Enter WhatsApp number!" }, { pattern: /^\d{10}$/, message: "Enter a valid 10-digit number!" }]}> 
+          <Input placeholder="Mobile Number" />
         </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: "Please enter your full name!" }]}
-        >
-          <Input type="password" />
+        <Form.Item name="password" rules={[{ required: true, message: "Enter your password!" }]}> 
+          <Input placeholder="Create Password" type="password" />
         </Form.Item>
-        <Form.Item
-          name="confirmPassword"
-          rules={[{ required: true, message: "Please enter your full name!" }]}
-        >
-          <Input type="password" />
+        <Form.Item name="confirmPassword" rules={[{ required: true, message: "Re-enter your password!" }]}> 
+          <Input placeholder="Re-Enter Password" type="password" />
         </Form.Item>
         <Form.Item>
           <Button type="primary" htmlType="submit" className="register-button">
             Register
           </Button>
         </Form.Item>
+        <div className="login-link">
+          <p>Already Have An Account? <a href="/login">Login</a></p>
+        </div>
       </Form>
     </div>
   );
